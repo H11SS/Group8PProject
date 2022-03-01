96de5c734ae745a176e1dd16f134ec269b443732
H11SS <muhdhaziqsalim@gmail.com>
2022-03-01 10:39:17 +0800

    Initial commit

---
+++ header.py
@@ -0,0 +1,21 @@
+import requests
+
+url = 'https://www.premier-football.net/teams/english-premier-league.html'
+r = requests.get(url)
+print(r.text)
+print("Status code:")
+print("\t *", r.status_code)
+
+h = requests.head(url)
+print("Header:")
+print("**********")
+for x in h.headers:
+    print("\t ", x, ":", h.headers[x])
+print("**********")
+
+headers = {
+    'User-Agent': 'Mobile'
+}
+url2 = 'http://httpbin.org/headers'
+rh = requests.get(url2, headers=headers)
+print(rh.text)
